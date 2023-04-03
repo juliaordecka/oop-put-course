6a0c7209a3d4d6d79ab5615076397d5cd503500d
juliaordecka <126468288+juliaordecka@users.noreply.github.com>
2023-04-03 13:22:34 +0200

    Add files via upload

---
+++ lesson-05/ConsoleApplication5.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class myName
+{
+public:
+	virtual string Name() = 0;
+};
+//kontstruntor ustawiajacy wartosc pola nick
+class nickname : public myName //prywatne pole nick
+{
+public:
+	string Name() //metoda wypisuje wartosc pola nick
+	{
+		return "Julka";
+	}
+};
+//analogicznie
+class fullname : public myName
+{
+public:
+	string Name()
+	{
+		return "Julia Ordecka";
+	}
+};
+
+int main()
+{
+	nickname name1;
+	fullname name2;
+	myName* ptr;
+	ptr = &name1;
+	cout << "Nickname - " << ptr->Name();
+	ptr = &name2;
+	cout << "\nFull name - " << ptr->Name();
+	return 0;
+}
+
